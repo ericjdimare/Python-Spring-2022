a0a91e60ef87e3c01334403c5ba40facdd1370d2
Eric <ericjdimare@gmail.com>
2022-01-18 14:40:21 -0500

    print() functions added to display general information

---
+++ FIRST.py
@@ -0,0 +1,9 @@
+first_name = "Eric"
+last_name = "DiMare"
+full_name = first_name + " " + last_name
+
+print('My name is', full_name,'.' )
+print("I attended part of UNH's coding bootcamp and have taken 2 other programming courses at MCC.")
+
+favorite_number = int(input("Enter your favorite number: "))
+print('My favorite number is', favorite_number)
